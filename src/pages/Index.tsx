3d7346e33ae1754e75e23576a39e36b1c82f61f6
Nitanshu Joshi <nitanshuj138@gmail.com>
2025-09-16 04:56:19 +0530

    Add debug code for environment variables

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Navigation from "../components/Navigation";
 import Hero from "../components/Hero";
 import Gallery from "../components/Gallery";
@@ -5,6 +6,12 @@ import AIGenerated from "../components/AIGenerated";
 import About from "../components/About";
 
 const Index = () => {
+  // Temporary debug code to check environment variables
+  useEffect(() => {
+    console.log('Cloud name from env:', import.meta.env.VITE_CLOUDINARY_CLOUD_NAME);
+    console.log('All env vars:', import.meta.env);
+  }, []);
+
   return (
     <div className="min-h-screen bg-background text-foreground">
       <Navigation />
